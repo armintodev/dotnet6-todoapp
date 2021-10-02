96452161b292d08481f0a63fe955793aa1668e15
arminatwork <workwitharmin7@gmail.com>
2021-10-03 01:12:07 +0330

    add base request record

---
+++ dotnet6-training/Models/BaseRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace dotnet6_training.Models;
+
+public record BaseRequest();
--- dotnet6-training/Models/CreateTodoRequest.cs
+++ dotnet6-training/Models/CreateTodoRequest.cs
@@ -1,4 +1,4 @@
 ﻿namespace dotnet6_training.Models;
 
-public record CreateTodoRequest(string Title, string Description);
-public record EditTodoRequest(int Id, string Title, string Description);
+public record CreateTodoRequest(string Title, string Description) : BaseRequest();
+public record EditTodoRequest(int Id, string Title, string Description) : BaseRequest();
