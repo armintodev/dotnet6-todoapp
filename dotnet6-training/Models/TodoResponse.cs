088a13657b31fa95f9aa5c606c28271c0926cd0e
arminatwork <workwitharmin7@gmail.com>
2021-10-03 01:13:07 +0330

    add implicit operators to implicit cast in Create/Edit TodoRequest for TodoResponse

--- dotnet6-training/Models/TodoResponse.cs
+++ dotnet6-training/Models/TodoResponse.cs
@@ -1,3 +1,21 @@
 ﻿namespace dotnet6_training.Models;
 
-public record TodoResponse(int Id, string Title, string Description, string CreateDate);
+public record TodoResponse(int Id, string Title, string Description, string CreateDate)
+{
+    public static implicit operator TodoResponse(CreateTodoRequest request)
+    {
+        return new(0,
+            request.Title,
+            request.Description,
+            DateTime.Now.ToString());
+    }
+
+    public static implicit operator TodoResponse(EditTodoRequest request)
+    {
+        return new(request.Id,
+            request.Title,
+            request.Description,
+            DateTime.Now.ToString());
+    }
+
+}
